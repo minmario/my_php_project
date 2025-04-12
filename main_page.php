f21c56cb7eff3c4251f67947ecbe8786c89169b5
minmario <minmario@korea.ac.kr>
2025-04-13 03:31:36 +0900

    오류수정

--- main_page.php
+++ main_page.php
@@ -5,11 +5,13 @@
 <title>릴레이션 선택</title>
 </head>
 <body>
-  <form method="post" name="main_page.php" action="select_page.php">
-<h2> 릴레이션을 선택하시오.(구매자,대리인,원자재,주문,계약)</h2>
-<form>
-<p><label>릴레이션을 선택하시오.(구매자,대리인,원자재,주문,계약)<input type = "text" name="relation"/></label>></p>
-<input type = "submit" value = "보내기">
-</form>
+  <form method="post" name="main_page" action="select_page.php">
+    <h2>릴레이션을 선택하시오.(구매자, 대리인, 원자재, 주문, 계약)</h2>
+    <p>
+      <label for="relation">릴레이션을 선택하시오:</label>
+      <input type="text" id="relation" name="relation"/>
+    </p>
+    <input type="submit" value="보내기">
+  </form>
 </body>
 </html>
